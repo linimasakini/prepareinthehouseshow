0de87173a159c86626f01891fba5c0109d7ec5ee
Linimasa <116949567+linimasakini@users.noreply.github.com>
2023-02-28 07:04:08 +0700

    Add files via upload

---
+++ movie-catalogue/specs/helpers/testFactories.js
@@ -0,0 +1,10 @@
+import LikeButtonInitiator from '../../src/scripts/utils/like-button-presenter';
+
+const createLikeButtonPresenterWithMovie = async (movie) => {
+  await LikeButtonInitiator.init({
+    likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    movie,
+  });
+};
+
+export { createLikeButtonPresenterWithMovie };
